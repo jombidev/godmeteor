891c7a9d77d1e8d65ce0005f3c1abf760f8ccdaf
jombi <jombi@duck.com>
2024-06-21 12:06:54 +0900

    Initial commit

---
+++ src/main/java/dev/jombi/godmeteor/GodmeteorApplication.java
@@ -0,0 +1,13 @@
+package dev.jombi.godmeteor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodmeteorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GodmeteorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jombi/godmeteor/GodmeteorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jombi.godmeteor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodmeteorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
