b39f3d5b8c9f18a7a0f58cdc11c867c1dab8de7d
kong2ys <yousung0525@naver.com>
2024-06-22 22:29:19 +0900

    fix :: UserDto lombok

--- src/main/java/dev/jombi/godmeteor/communtiy/SYS/dto/UserDto.java
+++ src/main/java/dev/jombi/godmeteor/communtiy/SYS/dto/UserDto.java
@@ -4,6 +4,8 @@ import dev.jombi.godmeteor.communtiy.JJE.Entity.User;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter
+@Setter
 public class UserDto {
     private Long id;
     private String name;
