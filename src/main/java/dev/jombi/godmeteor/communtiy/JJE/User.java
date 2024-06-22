5b624125fc5c4cd69cbd32a671b0705713fd7585
harp515 <128121328+harp515@users.noreply.github.com>
2024-06-22 19:07:23 +0900

    feat :: User.java

---
+++ src/main/java/dev/jombi/godmeteor/communtiy/JJE/User.java
@@ -0,0 +1,30 @@
+package dev.jombi.godmeteor.communtiy.JJE;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+    @PrePersist
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
--- src/main/java/dev/jombi/godmeteor/communtiy/JJE/one.java
+++
@@ -1,4 +0,0 @@
-package dev.jombi.godmeteor.communtiy.JJE;
-
-public class one {
-}
