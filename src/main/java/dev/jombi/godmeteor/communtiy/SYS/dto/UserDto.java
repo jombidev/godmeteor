c2df5d726abf47c1e585ca5be2f329c0e1a5d6ea
kong2ys <yousung0525@naver.com>
2024-06-22 22:26:40 +0900

    feat :: UserDto lombok add

--- src/main/java/dev/jombi/godmeteor/communtiy/SYS/dto/UserDto.java
+++ src/main/java/dev/jombi/godmeteor/communtiy/SYS/dto/UserDto.java
@@ -1,32 +1,11 @@
 package dev.jombi.godmeteor.communtiy.SYS.dto;
 
 import dev.jombi.godmeteor.communtiy.JJE.Entity.User;
+import lombok.Getter;
+import lombok.Setter;
 
 public class UserDto {
     private Long id;
     private String name;
 
-    public UserDto() {
-    }
-
-    public UserDto(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
