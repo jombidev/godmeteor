1a0668b1e34ea1f492b163c9b3405743806ff5c6
kong2ys <yousung0525@naver.com>
2024-06-22 19:12:24 +0900

    feat :: UserRepo add

--- src/main/java/dev/jombi/godmeteor/communtiy/SYS/repository/UserRepository.java
+++ src/main/java/dev/jombi/godmeteor/communtiy/SYS/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package dev.jombi.godmeteor.communtiy.SYS.repository;
 
-public class UserRepository {
+import dev.jombi.godmeteor.communtiy.JJE.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
