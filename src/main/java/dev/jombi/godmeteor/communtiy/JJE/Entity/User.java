54e1c72910627c4fd6a2b47ff3736ac6036724d9
harp515 <128121328+harp515@users.noreply.github.com>
2024-06-22 22:25:59 +0900

    fix :: User @Getter,@Setter 변경

--- src/main/java/dev/jombi/godmeteor/communtiy/JJE/Entity/User.java
+++ src/main/java/dev/jombi/godmeteor/communtiy/JJE/Entity/User.java
@@ -19,22 +19,6 @@ public class User {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @PrePersist
     protected void onCreate() {
         createdAt = LocalDateTime.now();
