a8606f30581f8c7525a2a5fa783149204382227a
kong2ys <yousung0525@naver.com>
2024-06-22 18:15:20 +0900

    feat :: make controller Package

--- src/main/java/dev/jombi/godmeteor/GodMeteorApplication.java
+++ src/main/java/dev/jombi/godmeteor/GodMeteorApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GodMeteorApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GodMeteorApplication.class, args);
+    public static void main(String[] args) { SpringApplication.run(GodMeteorApplication.class, args);
     }
 
 }
