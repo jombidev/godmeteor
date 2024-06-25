5d411d28ae7a8a66ab61ef68758d0004e39515f5
kong2ys <131360841+kong2ys@users.noreply.github.com>
2024-06-25 11:31:24 +0900

    fix :: Repo rename

--- src/main/java/dev/jombi/godmeteor/communtiy/SYS/repository/PostRepository.java
+++ src/main/java/dev/jombi/godmeteor/communtiy/SYS/repository/PostRepository.java
@@ -4,10 +4,8 @@ import dev.jombi.godmeteor.communtiy.JJE.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.reflect.Member;
 import java.util.List;
 
 @Repository
-public interface UserRepository extends JpaRepository<Post, Long> {
-    public List<Post> findByMember(Member member);
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
