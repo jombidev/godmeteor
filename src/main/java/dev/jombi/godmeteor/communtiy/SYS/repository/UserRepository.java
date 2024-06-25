2c1d9f81312aca94bb3e1140e233a95a6406a9b4
duck3866 <130462482+duck3866@users.noreply.github.com>
2024-06-25 09:49:17 +0900

    feat :: UserRepo @Repository update(kong2ys)

--- src/main/java/dev/jombi/godmeteor/communtiy/SYS/repository/UserRepository.java
+++ src/main/java/dev/jombi/godmeteor/communtiy/SYS/repository/UserRepository.java
@@ -2,6 +2,8 @@ package dev.jombi.godmeteor.communtiy.SYS.repository;
 
 import dev.jombi.godmeteor.communtiy.JJE.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
