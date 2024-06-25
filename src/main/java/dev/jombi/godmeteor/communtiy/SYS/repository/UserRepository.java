b1cae42ca83c4250d4a774c7c6dfd219aa6adac9
kong2ys <131360841+kong2ys@users.noreply.github.com>
2024-06-25 11:19:53 +0900

    feat :: UserRepo update

--- src/main/java/dev/jombi/godmeteor/communtiy/SYS/repository/UserRepository.java
+++ src/main/java/dev/jombi/godmeteor/communtiy/SYS/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package dev.jombi.godmeteor.communtiy.SYS.repository;
 
-import dev.jombi.godmeteor.communtiy.JJE.Entity.User;
+import dev.jombi.godmeteor.communtiy.JJE.Entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.lang.reflect.Member;
+import java.util.List;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<Post, Long> {
+    public List<Post> findByMember(Member member);
 }
