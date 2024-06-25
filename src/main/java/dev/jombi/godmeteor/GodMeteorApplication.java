5320a0879a3ed91b4de8315d57b2ae00828fadb3
harp515 <128121328+harp515@users.noreply.github.com>
2024-06-25 09:34:02 +0900

    feat :: GMApplication.java에 EnableJpaAuditing 추가

--- src/main/java/dev/jombi/godmeteor/GodMeteorApplication.java
+++ src/main/java/dev/jombi/godmeteor/GodMeteorApplication.java
@@ -2,8 +2,10 @@ package dev.jombi.godmeteor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GodMeteorApplication {
 
     public static void main(String[] args) { SpringApplication.run(GodMeteorApplication.class, args);
