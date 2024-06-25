a1d806abfc169ddf81043c3ff8792ba28963dfd0
jombi <jombi@duck.com>
2024-06-25 21:51:14 +0900

    chore :: reformat GodMeteorApplication code

--- src/main/java/dev/jombi/godmeteor/GodMeteorApplication.java
+++ src/main/java/dev/jombi/godmeteor/GodMeteorApplication.java
@@ -8,7 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class GodMeteorApplication {
 
-    public static void main(String[] args) { SpringApplication.run(GodMeteorApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(GodMeteorApplication.class, args);
     }
 
 }
