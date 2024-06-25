83d0ac045a942d7008b826de8d179b4a54570f1f
jombi <jombi@duck.com>
2024-06-25 20:07:23 +0900

    feat :: in global, ExceptionHandlerAdvice

--- src/main/java/dev/jombi/godmeteor/global/exception/GlobalExceptionCode.java
+++ src/main/java/dev/jombi/godmeteor/global/exception/GlobalExceptionCode.java
@@ -3,7 +3,9 @@ package dev.jombi.godmeteor.global.exception;
 import org.springframework.http.HttpStatus;
 
 public enum GlobalExceptionCode implements ExceptionDetail {
-    INTERNAL_SERVER_ERROR("내부 서버 에러", HttpStatus.INTERNAL_SERVER_ERROR);
+    INTERNAL_SERVER_ERROR("내부 서버 에러", HttpStatus.INTERNAL_SERVER_ERROR),
+    PARAMETER_NOT_MATCH("파라미터 불일치", HttpStatus.BAD_REQUEST),
+    ;
 
     private final String message;
     private final HttpStatus status;
--- src/main/java/dev/jombi/godmeteor/global/handler/ExceptionHandlerAdvice.java
+++ src/main/java/dev/jombi/godmeteor/global/handler/ExceptionHandlerAdvice.java
@@ -5,20 +5,26 @@ import dev.jombi.godmeteor.global.exception.ExceptionResponse;
 import dev.jombi.godmeteor.global.exception.GlobalExceptionCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
-    @ExceptionHandler
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> exception(Exception e) {
         log.error("Error while processing request: ", e);
         return ExceptionResponse.of(GlobalExceptionCode.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(CustomException.class)
     public ResponseEntity<ExceptionResponse> customException(CustomException e) {
         return ExceptionResponse.of(e.getDetail());
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ExceptionResponse> missingParameter(MissingServletRequestParameterException ignored) {
+        return ExceptionResponse.of(GlobalExceptionCode.PARAMETER_NOT_MATCH);
     }
 }
